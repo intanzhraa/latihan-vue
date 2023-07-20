803e442ce7e997d3cebe2acd74b2c78265e241a7
iqbalzeeee <iqbalnauval920@gmail.com>
2023-07-20 15:36:33 +0700

    latihan vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = "hello world"
+const rawHtml = "<li>halooo</li>"
+const link = "https://smkassalaambandung.sch.id/"
+let a=10
+let b=5
+let c =a+b
+</script>
+
+<template>
+  <div v-bind:id="dynamicId" id="app">
+   <h1>{{ message }}</h1>
+
+   <span style="color: red" v-html="rawHtml"></span>
+
+    <button :disabled="isButtonDisabled">button</button><br>
+    <a v-bind:href="link">situs assalaam</a><br>
+    bil1={{ a }}<br>
+    bil2={{ b }}<br>
+    hasil={{ c }}
+
+  </div>
+
+</template>
